11241653ec0314c77007161c8cc27bc366c32fd0
alex-jones <abjhoops@gmail.com>
2023-01-27 20:47:40 -0700

    display name and model on ship details page

--- src/pages/ShipDetail/ShipDetail.jsx
+++ src/pages/ShipDetail/ShipDetail.jsx
@@ -1,9 +1,27 @@
-const ShipDetail = () => {
+import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+import { getDetails } from '../../services/sw-api'
+
+const ShipDetails = () => {
+  const [starshipDetails, setStarshipDetails] = useState({})
+  const location = useLocation()
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const shipData = await getDetails(location.state.starShip.url)
+      setStarshipDetails(shipData)
+    }
+    fetchDetails()
+  }, [location.state.starShip.url])
+
   return ( 
-    <main>
-	    <h1>Ship Detail</h1>
-    </main>
+    <>
+      <h3>Ship Details</h3>
+      <h3>{starshipDetails.name}</h3>
+      <h4>Model: { starshipDetails.model }</h4>
+    </>
   )
 }
 
-export default ShipDetail
+export default ShipDetails;
--- src/pages/ShipList/ShipList.jsx
+++ src/pages/ShipList/ShipList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getAllStarships } from "../../services/sw-api"
 import { Link } from 'react-router-dom'
+import ShipDetail from "../ShipDetail/ShipDetail"
 
 const ShipList = () => {
   const [ starShipList, setStarShipList ] = useState([])
