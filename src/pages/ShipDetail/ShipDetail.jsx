50dec35eaffc65e2888254f06a7c1d039048c4c3
alex-jones <abjhoops@gmail.com>
2023-01-27 20:50:16 -0700

    add return link to ship details

--- src/pages/ShipDetail/ShipDetail.jsx
+++ src/pages/ShipDetail/ShipDetail.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { getDetails } from '../../services/sw-api'
 
@@ -20,6 +21,7 @@ const ShipDetails = () => {
       <h3>Ship Details</h3>
       <h3>{starshipDetails.name}</h3>
       <h4>Model: { starshipDetails.model }</h4>
+      <Link to={'/'}>RETURN</Link>
     </>
   )
 }
